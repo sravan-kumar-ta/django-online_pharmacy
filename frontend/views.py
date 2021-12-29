8818f1d67e0afa34266dd4b96abe14e3598e1581
sravan-kumar-ta <sravan0095@gmail.com>
2021-12-29 20:03:32 +0530

    paginator done

--- frontend/views.py
+++ frontend/views.py
@@ -19,9 +19,19 @@ def category_medicines(request, slug):
     medicines = Medicine.objects.filter(is_active=True, category=category)
     all_categories = Category.objects.all()
 
+    paginator = Paginator(medicines, 3)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        medicines_by_page = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        medicines_by_page = paginator.page(paginator.num_pages)
+
     context = {
         'category': category,
-        'medicines': medicines,
+        'medicines': medicines_by_page,
         'categories': all_categories,
     }
     return render(request, 'frontend/pages/category_products.html', context)
