4b4cbce8b23170b31275d78aa15d571247069f8d
sravan-kumar-ta <sravan0095@gmail.com>
2021-12-31 12:41:31 +0530

    customer registration done

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from frontend import views, customer_views
+from frontend.forms import LoginForm
+from django.contrib.auth import views as auth_views
 
 app_name = 'customer'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('categories/', views.categories, name='categories'),
+    path('sign_up/', views.add_customer, name="registration"),
+    path('log_in/', views.do_login, name="login"),
+
     path('<slug:slug>/', customer_views.category_medicines, name="medicine-list"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from account.EmailBackend import EmailBackEnd
+from account.models import CustomUser
 from medicines.models import Category
 
 
@@ -11,3 +16,38 @@ def home(request):
 def categories(request):
     all_categories = Category.objects.all()
     return render(request, 'frontend/pages/categories.html', {'categories': all_categories})
+
+
+def add_customer(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('f_name')
+        last_name = request.POST.get('l_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        customer = CustomUser.objects.create_user(first_name=first_name, last_name=last_name, username=username,
+                                                  email=email, password=password, user_type=3)
+        customer.save()
+        messages.success(request, "Congratulations! Registration Successful!")
+        return redirect('customer:registration')
+
+    else:
+        return render(request, 'frontend/pages/auth/register.html')
+
+
+def do_login(request):
+    if request.method != 'POST':
+        return render(request, 'frontend/pages/auth/login.html')
+    else:
+        try:
+            user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),
+                                             password=request.POST.get("password"))
+            if user is not None:
+                login(request, user)
+                return redirect('customer:home')
+            else:
+                messages.error(request, "Invalid Login")
+                return redirect('customer:login')
+        except:
+                messages.error(request, "Invalid Login")
+                return redirect('customer:login')
